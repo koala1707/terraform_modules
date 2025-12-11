3d3a2e1b6be170edc6db572281100bd8d8bbbea3
koala1707 <m3tpfbzr@gmail.com>
2025-12-11 12:54:22 +1030

    deleted global because tf tried to load modules and global at the same time

---
+++ modules/resource-group/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type = string
+  default = "iacdeployment"
+}
+
+variable "env" {
+  type = string
+  default = "prod"
+}
+
+variable "location" {
+  type = string
+  default = "australiaeast"
+}
