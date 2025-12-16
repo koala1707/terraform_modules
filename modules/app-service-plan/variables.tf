e8a0a9b220b53c1c375decf3c4f1351a55d56f72
koala1707 <m3tpfbzr@gmail.com>
2025-12-16 19:18:53 +1030

    fix/make variable descriptions string

--- modules/app-service-plan/variables.tf
+++ modules/app-service-plan/variables.tf
@@ -1,14 +1,14 @@
 variable "name" {
   type = string
-  description = name of the App Service Plan
+  description = "name of the App Service Plan"
 }
 
 variable "location" {
   type = string
-  description = Azure region of the App Service Plan
+  description = "Azure region of the App Service Plan"
 }
 
 variable "sku" {
   type = string
-  description = sku of the App Service Plan
+  description = "Sku of the App Service Plan"
 }
