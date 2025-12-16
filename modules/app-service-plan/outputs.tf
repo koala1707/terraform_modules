530f063e43b55c53c3734942c6198bd0c2c92ec3
koala1707 <m3tpfbzr@gmail.com>
2025-12-16 20:10:29 +1030

    feat/add location in app service plan outputs.tf

--- modules/app-service-plan/outputs.tf
+++ modules/app-service-plan/outputs.tf
@@ -1,3 +1,9 @@
-output id {
+output "id" {
   value = azurerm_service_plan.asp.id
+  description = "id of the App Service Plan"
+}
+
+output "location" {
+  value = azurerm_service_plan.asp.location
+  description = "location of the App Service Plan"
 }
