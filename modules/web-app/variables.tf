ad5a48d813d8954c4445a9a28bdddcc765d4644b
koala1707 <m3tpfbzr@gmail.com>
2025-12-16 19:00:21 +1030

    fix/make variable definition string

--- modules/web-app/variables.tf
+++ modules/web-app/variables.tf
@@ -1,29 +1,29 @@
-variable env {
+variable "env" {
   type = string
   default = "dev"
   description = "Deployment environment (e.g. dev, stg, prod)"
 }
 
-variable resource_group_name {
+variable "resource_group_name" {
   type = string
   description = "name of the resrouce group"
 }
 
-variable location {
+variable "location" {
   type = string
   description = "Azure region for deployment"
 }
 
-variable service_plan_id {
+variable "service_plan_id" {
   type = string
   description = "id of the App Service Plan"
 }
 
-variable webapp_name {
+variable "webapp_name" {
   type = string
 }
 
-variable project_name {
+variable "project_name" {
   type = string
   description = "Name of the project for a tag"
 }
