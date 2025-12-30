3e0592740df6c031d43fd3bf7f3cd3e6afc8560e
koala1707 <m3tpfbzr@gmail.com>
2025-12-30 12:33:01 +1030

    add variables for storage container

---
+++ modules/storage_container/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type = string
+  description = "name of the storage container"
+}
+
+variable "storage_account_id" {
+  type = string
+  description = "id of the storage account"
+}
+
