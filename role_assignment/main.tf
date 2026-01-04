42d2c675915eaefb772d5ef1a60311dc7a0a1c54
koala1707 <m3tpfbzr@gmail.com>
2026-01-04 20:45:07 +1030

    feat/add role assignment

---
+++ role_assignment/main.tf
@@ -0,0 +1,9 @@
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "primary" {}
+
+resource "azurerm_role_assignment" "" {
+  scope                = var.scope
+  role_definition_name = var.role_definition
+  principal_id         = var.principal_id
+}
---
+++ role_assignment/variables.tf
@@ -0,0 +1,22 @@
+variable "role_definition" {
+  type = string
+  description = "rbac role name to assign"
+  validation {
+    condition = ([
+      "Reader",
+      "Key Vault Administrator",
+      "Key Vault Secrets User",
+    ], var.role_definition)
+    error_message = "role_definition must be a valid azure built-in role name"
+  }
+}
+
+variable "scope" {
+  type        = string
+  description = "scope at which the role assignment applies"
+}
+
+variable "principal_id" {
+  type        = string
+  description = "object ID of the principal"
+}
