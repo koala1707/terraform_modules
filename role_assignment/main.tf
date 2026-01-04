6a15491d6ff442330b705c4359ff04d9de15455d
koala1707 <m3tpfbzr@gmail.com>
2026-01-04 20:52:43 +1030

    fix/add a name of role_assignment module

--- role_assignment/main.tf
+++ role_assignment/main.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_subscription" "primary" {}
 
-resource "azurerm_role_assignment" "" {
+resource "azurerm_role_assignment" "rbac" {
   scope                = var.scope
   role_definition_name = var.role_definition
   principal_id         = var.principal_id
